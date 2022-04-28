77477580dc29f2e3cd0abff08abb4e376507f3a3
gwunleong lee <gnleong@gmail.com>
2022-04-28 09:08:36 +0800

    add terraform ecr foo creation

--- terraform/public_ecr.tf
+++ terraform/public_ecr.tf
@@ -35,3 +35,12 @@ data "aws_iam_policy_document" "ecr_policy_doc" {
     }
   }
 }
+
+resource "aws_ecr_repository" "foo" {
+  name                 = "bar"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
